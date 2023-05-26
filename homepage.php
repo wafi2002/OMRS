10a41db1dcd350c463777b5e1e37d96186766a4a
kiki <aliff.iman11@yahoo.com>
2023-05-26 20:10:04 +0800

    add 2 file

---
+++ homepage.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mainpage.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <header>
+        <div class="logo-box">
+            <div class="logo-container">
+              <img src="logopahang.png" alt="Logo" class="logo">
+              <h1 class="logo-text">E-MUNAKAHAT</h1>
+            </div>
+            <div class="text-container">
+              <p class="text1">Sistem Maklumat Perkahwinan Pahang</p>
+              <div class="lineheader"></div>
+              <p class="text2">Jabatan Agama Islam Pahang</p>
+            </div>
+          </div>
+    </header> 
+
+
+    <div class="Profile">
+        <img src="icon.png" alt="" class="profileicon">
+
+        <div class="profile">
+            <div class="profilename">
+                <p class="profile-name">Name : </p><p class="profile-name">Alex</p>
+            </div>
+            <div class="profileid">
+                <p class="profile-id">ID : </p><p class="profile-id">0202541230</p>
+            </div>
+        </div>
+        
+    </div>
+
+    <div class="menu-box">
+        <nav class="menu">
+            <ol>
+                <li class="menu-item-head" id="li"><a href="#0" id="a">Home</a></li>
+                <li class="menu-item-head" id="li"><a href="#0" id="a">About</a></li>
+                <li class="menu-item-head" id="li"><a href="#0" id="a">Profile</a></li>
+                <li class="menu-item">
+                    <li>Permohonan Kursus <br>Perkahwinan</li>
+                    <ol class="sub-menu">
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Maklumat-Pemohon">Maklumat Pemohon</a></li>
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Status-Permohonan">Status Permohonan</a></li>
+                    </ol>
+                </li>
+                <li class="menu-item">
+                    <li>Permohonan Perkahwinan</li>
+                    <ol class="sub-menu">
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Maklumat-Perkahwinan">Maklumat Perkahwinan</a></li>
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Dokumen-Sokongan">Dokumen Sokongan</a></li>
+                    </ol>
+                </li>
+                <li class="menu-item">
+                    <li>Pendaftaran Perkahwinan</li>
+                    <ol class="sub-menu">
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Maklumat-Pemohon">Jenis Perkahwinan</a></li>
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="Status-Permohonan">Sijil & Kad Perkahwinan</a></li>
+                    </ol>
+                </li>
+                <li class="menu-item" id="li"><a href="#0" id="a">Khidmat Nasihat</a></li>
+                <li class="menu-item has-submenu">
+                    <span>Insentif</span>
+                    <ol class="sub-menu">
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="syarat-kelayakan">Syarat Kelayakan</a></li>
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="mohon">Mohon</a></li>
+                        <li class="menu-item" id="li"><a  id="a" href="#0" class="submenu-link" data-content="result">Result</a></li>
+                    </ol>
+                </li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="content">
+
+    </div>
+
+    
+    <script src="homepage.js"></script>
+</body>
+</html>
